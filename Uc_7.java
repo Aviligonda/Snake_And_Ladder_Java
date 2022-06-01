11942a22494c8b23408d76fa6279a611fea33623
aviligonda <aviligondasreenivasulu6086@gmail.com>
2022-06-01 20:10:34 +0530

    SnakeAndLadder

---
+++ Uc_7.java
@@ -0,0 +1,124 @@
+public class Uc_7 {
+
+    public static final int NO_PLAY = 0;
+
+    public static final int LADDER = 1;
+    public static final int SNAKE = 2;
+    public static final int START = 0;
+    public static int player1=0;
+public static int player2=0;
+    public static void SnakeAndLadder() {
+
+        int count = 0;
+
+        int pos = 0;
+        System.out.println("Starting position of the player is : " + START);
+        while (pos <= 100) {
+            int roll = (int) (Math.random() * 6) + 1;
+            System.out.println("Number on rolled dice is :" + roll);
+            int con = (int) (Math.random() * 3);
+            System.out.println("Condition :" + con);
+            switch (con) {
+                case NO_PLAY -> {
+                    System.out.println("New position is :" + pos);
+                    count++;
+                    break;
+                }
+                case LADDER -> {
+                    pos = pos + roll;
+                    System.out.println("New position is :" + pos);
+                    count++;
+                    break;
+                }
+                case SNAKE -> {
+                    pos = pos - roll;
+                    if (pos >= 0) {
+                        System.out.println("New position is :" + pos);
+                    } else {
+                        pos = 0;
+                        System.out.println("New position is :" + pos);
+                    }
+                    count++;
+                    break;
+                }
+                default -> {
+                    System.out.println("Default");
+                    break;
+                }
+            }
+            System.out.println();
+            if (pos == 100) {
+                System.out.println("------------------");
+                System.out.println("   PLAYER 1 WON    ");
+                break;
+            }
+
+        }
+        System.out.println("No of time to play to reach the win position ="+ count);
+        player1=count;
+        System.out.println(player1);
+
+    }
+
+    public static void Ladder() {
+
+        int count = 0;
+
+        int pos = 0;
+        System.out.println("Starting position of the player is : " + START);
+        while (pos <= 100) {
+            int roll = (int) (Math.random() * 6) + 1;
+            System.out.println("Number on rolled dice is :" + roll);
+            int con = (int) (Math.random() * 3);
+            System.out.println("Condition :" + con);
+            switch (con) {
+                case NO_PLAY -> {
+                    System.out.println("New position is :" + pos);
+                    count++;
+                    break;
+                }
+                case LADDER -> {
+                    pos = pos + roll;
+                    System.out.println("New position is :" + pos);
+                    count++;
+                    break;
+                }
+                case SNAKE -> {
+                    pos = pos - roll;
+                    if (pos >= 0) {
+                        System.out.println("New position is :" + pos);
+                    } else {
+                        pos = 0;
+                        System.out.println("New position is :" + pos);
+                    }
+                    count++;
+                    break;
+                }
+                default -> {
+                    System.out.println("Default");
+                    break;
+                }
+            }
+            System.out.println();
+            if (pos == 100) {
+                System.out.println("------------------");
+                System.out.println("   PLAYER 2 WON    ");
+                break;
+            }
+
+        }
+        System.out.println("No of time to play to reach the win position =" + count);
+        player2=count;
+
+    }
+
+    public static void main(String[] args) {
+        SnakeAndLadder();
+        Ladder();
+if(player1>player2){
+    System.out.println("Player1 is win");
+}else {
+    System.out.println("Player2 is win");
+}
+    }
+}
